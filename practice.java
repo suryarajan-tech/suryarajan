67ce143a2ec70be5aa99ed5a47b11554daa65633
suryarajan-tech <84022301+suryarajan-tech@users.noreply.github.com>
2023-10-15 12:15:33 +0530

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,9 @@
+public class PrintPattern {
+    public static void main(String[] args) {
+        String input = "we are good programmers";
+        StringBuilder reversedWord = new StringBuilder(input).reverse();
+            
+        System.out.print(reversedword2.toString() + " ");
+    }
+}
+
